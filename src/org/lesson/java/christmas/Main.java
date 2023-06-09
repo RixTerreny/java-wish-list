802e09d30357769db51d942a36ec294a48513f0d
RixTerreny <riccardotrave43@gmail.com>
2023-06-09 17:28:02 +0200

    Exercise ended with bonus

--- src/org/lesson/java/christmas/Main.java
+++ src/org/lesson/java/christmas/Main.java
@@ -1,12 +1,25 @@
 package org.lesson.java.christmas;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         ArrayList <String> gifts = new ArrayList<>();
+
+        File fileGiftsPath = new File("gifts-file.txt");
+
+        Scanner reader = new Scanner(fileGiftsPath);
+
+        while (reader.hasNextLine()) {
+            String data = reader.nextLine();
+            gifts.add(data);
+            System.out.println(gifts);
+        }
+
+        reader.close();
 
         do {
             System.out.println("Inserisci un regalo: ");
@@ -15,6 +28,9 @@ public class Main {
             System.out.println("Oggetti nella lista: " + gifts.size() + "\n");
             System.out.println("Vuoi inserire un nuovo regalo?? 1 si, 2 no");
             if (scanner.nextInt()== 2){
+                FileWriter myWriter = new FileWriter(fileGiftsPath);
+                myWriter.write(String.valueOf(gifts));
+                myWriter.close();
                 break;
             }
             scanner.nextLine();
